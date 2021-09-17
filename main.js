29c35521de6801074a1a3478cdb8eb7e056f9563
Kheer-Thakkar <77600784+Kheer-Thakkar@users.noreply.github.com>
2021-09-17 21:03:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function preload(){
+    video=createVideo('video.mp4');
+    video.hide();
+}
+
+function setup(){
+    canvas=createCanvas(400, 400);
+    canvas.center();
+    video.hide();
+}
+function draw(){}
